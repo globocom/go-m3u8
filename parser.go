259468d79de2bbd43320482393a621ad52ea2e5a
kalil.cazes <kalil.cazes@g.globo>
2025-02-05 12:04:26 -0300

    Adiciona estrutura de lista duplamente encadeada

--- parser.go
+++ parser.go
@@ -2,12 +2,30 @@ package go_m3u8
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
-	"os"
 	"regexp"
 	"strings"
 )
+
+var (
+	ErrOpenPlaylist = errors.New("failed to open playlist")
+	ErrParseLine    = errors.New("failed to parse line")
+)
+
+type Source interface {
+	io.ReadCloser
+}
+type Playlist struct {
+	Head, Tail *Node
+	Elements   []Node
+}
+
+type Node struct {
+	Tags       any
+	Prev, Next *Node
+}
 
 type StreamInf struct {
 	Codecs           []string
@@ -16,11 +34,6 @@ type StreamInf struct {
 	Resolution       string
 	FrameRate        string
 	URI              string
-}
-
-type MasterPlaylist struct {
-	Variants []StreamInf
-	Version  string
 }
 
 type DateRange struct {
@@ -41,193 +54,157 @@ type ProgramDateTime struct {
 	DateTime string
 }
 
-type MediaPlaylist struct {
-	DateRanges            []DateRange
-	ProgramsDateTime      []ProgramDateTime
-	Segments              []Segment
-	Version               string
-	MediaSequence         string
-	DiscontinuitySequence string
-	TargetDuration        string
-}
+func ParsePlaylist(src Source) (*Playlist, error) {
+	var segment *Segment
+	var streamInf *StreamInf
+	playlist := new(Playlist)
 
-type Parser interface {
-	Parse(reader io.Reader) error
-}
-
-func ParseMediaPlaylist(filename string) (*MediaPlaylist, error) {
-	m := new(MediaPlaylist)
-	playlist, err := os.Open(filename)
-	if err != nil {
-		return nil, fmt.Errorf("error opening playlist: %v", err)
-	}
-
-	scanner := bufio.NewScanner(playlist)
-	var lines []string
-	uriIndex := 0
+	scanner := bufio.NewScanner(src)
+	defer src.Close()
 	for scanner.Scan() {
-		line := scanner.Text()
-
+		line := strings.TrimSpace(scanner.Text())
 		switch {
 		case strings.HasPrefix(line, "#EXT-X-VERSION:"):
-			version := m.parseVersion(line)
-			m.Version = version
+			version, err := playlist.parseVersion(line)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse version: %w", err)
+			}
+			playlist.Insert(&Node{Tags: version})
 
 		case strings.HasPrefix(line, "#EXT-X-TARGETDURATION:"):
-			targetDuration := m.parseTargetDuration(line)
-			m.TargetDuration = targetDuration
+			targetDuration, err := playlist.parseTargetDuration(line)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse target duration: %w", err)
+			}
+			playlist.Insert(&Node{Tags: targetDuration})
 
 		case strings.HasPrefix(line, "#EXT-X-MEDIA-SEQUENCE:"):
-			mediaSequence := m.parseMediaSequence(line)
-			m.MediaSequence = mediaSequence
+			mediaSequence, err := playlist.parseMediaSequence(line)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse media sequence: %w", err)
+			}
+			playlist.Insert(&Node{Tags: mediaSequence})
 
 		case strings.HasPrefix(line, "#EXT-X-PROGRAM-DATE-TIME"):
-			lines = append(lines, scanner.Text())
-			for range lines {
-				programDateTime := m.parseProgramDateTime(line)
-				m.ProgramsDateTime = append(m.ProgramsDateTime, *programDateTime)
-				lines = []string{}
-			}
+			programDateTime := playlist.parseProgramDateTime(line)
+			playlist.Insert(&Node{Tags: programDateTime})
 
 		case strings.HasPrefix(line, "#EXT-X-DATERANGE:"):
-			lines = append(lines, scanner.Text())
-			for range lines {
-				dateRange := m.parseDateRange(line)
-				m.DateRanges = append(m.DateRanges, *dateRange)
-				lines = []string{}
-			}
+			dateRange := playlist.parseDateRange(line)
+			playlist.Insert(&Node{Tags: dateRange})
 
 		case strings.HasPrefix(line, "#EXTINF:"):
-			lines = append(lines, scanner.Text())
-			for range lines {
-				duration := m.parseSegmentDuration(line)
-				m.Segments = append(m.Segments, *duration)
-				lines = []string{}
+			segment = playlist.parseSegmentDuration(line)
+
+		case strings.HasSuffix(line, ".ts"):
+			if segment != nil {
+				segment.URI = line
+				playlist.Insert(&Node{Tags: segment})
+				segment = nil
 			}
-
-		case strings.Contains(line, ".ts"):
-			m.Segments[uriIndex].URI = line
-			uriIndex++
-		}
-
-	}
-	return m, nil
-}
-
-func ParseMasterPlaylist(filename string) (*MasterPlaylist, error) {
-	m := new(MasterPlaylist)
-	variant := new(StreamInf)
-	playlist, err := os.Open(filename)
-	if err != nil {
-		return nil, fmt.Errorf("error opening playlist: %v", err)
-	}
-
-	scanner := bufio.NewScanner(playlist)
-	var lines []string
-	uriIndex := 0
-
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		switch {
-		case strings.HasPrefix(line, "#EXT-X-VERSION:"):
-			version := m.parseVersion(line)
-			m.Version = version
-
 		case strings.HasPrefix(line, "#EXT-X-STREAM-INF:"):
-			lines = append(lines, scanner.Text())
-			for range lines {
-				variant = m.parseStreamInf(line)
-				m.Variants = append(m.Variants, *variant)
-				lines = []string{}
+			streamInf = playlist.parseStreamInf(line)
+
+		case strings.HasSuffix(line, ".m3u8"):
+			if streamInf != nil {
+				streamInf.URI = line
+				playlist.Insert(&Node{Tags: streamInf})
+				streamInf = nil
 			}
-		case strings.Contains(line, ".m3u8"):
-			m.Variants[uriIndex].URI = line
-			uriIndex++
 		}
 	}
 
-	return m, nil
+	if err := scanner.Err(); err != nil {
+		return nil, ErrOpenPlaylist
+	}
+
+	return playlist, nil
 }
 
-func (m *MediaPlaylist) parseSegmentDuration(line string) *Segment {
+func (p *Playlist) parseSegmentDuration(line string) *Segment {
 	segment := new(Segment)
-	duration := strings.Split(line, ":")[1]
-	duration = strings.Split(duration, ",")[0]
-	segment.Duration = duration
+	parts := strings.Split(line, ":")
+	if len(parts) > 1 {
+		duration := strings.Split(parts[1], ",")[0]
+		segment.Duration = strings.TrimSpace(duration)
+	}
+
 	return segment
 }
 
-func (m *MediaPlaylist) parseProgramDateTime(line string) *ProgramDateTime {
+func (p *Playlist) parseProgramDateTime(line string) *ProgramDateTime {
 	programDateTime := new(ProgramDateTime)
-	time := strings.Split(line, ":")[1:4]
-	dateTime := strings.Join(time, ":")
-	programDateTime.DateTime = dateTime
+	parts := strings.SplitN(line, ":", 2)
+	if len(parts) > 1 {
+		programDateTime.DateTime = strings.TrimSpace(parts[1])
+	}
+
 	return programDateTime
 }
 
-func (m *MediaPlaylist) parseDateRange(line string) *DateRange {
-	params := splitParams(line)
+func (p *Playlist) parseDateRange(line string) *DateRange {
+	params := tagsToMap(line)
 	dateRange := new(DateRange)
+	dateRange.Scte35Mark = make(map[string]string)
 	for param, value := range params {
-		switch {
-		case param == "ID":
+		switch strings.ToUpper(param) {
+		case "ID":
 			dateRange.Id = value
-		case param == "START-DATE":
+		case "START-DATE":
 			dateRange.StartDate = value
-		case param == "END-DATE":
+		case "END-DATE":
 			dateRange.EndDate = value
-		case param == "PLANNED-DURATION":
+		case "PLANNED-DURATION":
 			dateRange.PlannedDuration = value
-		case param == "DURATION":
+		case "DURATION":
 			dateRange.Duration = value
-		case strings.HasPrefix(param, "SCTE"):
-			dateRange.Scte35Mark = m.parseSCTE(param, value)
+		default:
+			if strings.HasPrefix(param, "SCTE") {
+				scte := p.parseSCTE(param, value)
+				for k, v := range scte {
+					dateRange.Scte35Mark[k] = v
+				}
+			}
 		}
 	}
+
 	return dateRange
 }
 
-func (m *MediaPlaylist) parseSCTE(param, value string) map[string]string {
+func (p *Playlist) parseSCTE(param, value string) map[string]string {
 	scte := map[string]string{}
 	if strings.HasSuffix(param, "IN") {
-		scte = map[string]string{
-			"IN": value,
-		}
+		scte["IN"] = value
 	} else if strings.HasSuffix(param, "OUT") {
-		scte = map[string]string{
-			"OUT": value,
-		}
+		scte["OUT"] = value
 	}
+
 	return scte
 }
 
-func (m *MediaPlaylist) parseMediaSequence(line string) string {
-	mediaSequence := strings.Split(line, ":")[1]
-	m.MediaSequence = mediaSequence
-	return mediaSequence
+func (p *Playlist) parseMediaSequence(line string) (string, error) {
+	return parseLine(line)
 }
 
-func (m *MediaPlaylist) parseTargetDuration(line string) string {
-	duration := strings.Split(line, ":")[1]
-	m.TargetDuration = duration
-	return duration
+func (p *Playlist) parseTargetDuration(line string) (string, error) {
+	return parseLine(line)
 }
 
-func (m *MediaPlaylist) parseVersion(line string) string {
-	version := strings.Split(line, ":")[1]
-	m.Version = version
-	return version
+func (p *Playlist) parseVersion(line string) (string, error) {
+	return parseLine(line)
 }
 
-func (m *MasterPlaylist) parseVersion(line string) string {
-	version := strings.Split(line, ":")[1]
-	m.Version = version
-	return version
+func parseLine(line string) (string, error) {
+	parts := strings.Split(line, ":")
+	if len(parts) > 1 && parts[1] != "" {
+		return strings.TrimSpace(parts[1]), nil
+	}
+
+	return "", ErrParseLine
 }
 
-func (m *MasterPlaylist) parseStreamInf(line string) *StreamInf {
-	params := splitParams(line)
+func (p *Playlist) parseStreamInf(line string) *StreamInf {
+	params := tagsToMap(line)
 	variant := new(StreamInf)
 
 	for param, value := range params {
@@ -248,12 +225,38 @@ func (m *MasterPlaylist) parseStreamInf(line string) *StreamInf {
 }
 
 // https://regex101.com/r/0A2ulC/1
-func splitParams(line string) map[string]string {
+func tagsToMap(line string) map[string]string {
 	re := regexp.MustCompile(`([a-zA-Z\d_-]+)=("[^"]+"|[^",]+)`)
 	m := make(map[string]string)
 	for _, kv := range re.FindAllStringSubmatch(line, -1) {
 		k, v := kv[1], kv[2]
 		m[strings.ToUpper(k)] = strings.Trim(v, "\"")
 	}
+
 	return m
+}
+
+func (p *Playlist) Insert(node *Node) {
+	if p.Head == nil {
+		p.Head = node
+		p.Tail = node
+	} else {
+		node.Prev = p.Tail
+		p.Tail.Next = node
+		p.Tail = node
+	}
+
+	p.Elements = append(p.Elements, *node)
+}
+
+func (p *Playlist) Find(tag any) (*Node, bool) {
+	current := p.Head
+	for current != nil {
+		if current.Tags == tag {
+			return current, true
+		}
+		current = current.Next
+	}
+
+	return nil, false
 }
--- parser_test.go
+++ parser_test.go
@@ -1,206 +1,306 @@
 package go_m3u8_test
 
 import (
+	"os"
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	m3u8 "gitlab.globoi.com/webmedia/media-delivery-advertising/go-m3u8"
-	"testing"
 )
 
-func TestParseMasterPlaylist(t *testing.T) {
-	t.Parallel()
-	type testCaseParams struct {
-		name           string
-		path           string
-		masterPlaylist *m3u8.MasterPlaylist
-	}
+type fakeSource struct{}
 
+func (f fakeSource) Read(p []byte) (n int, err error) {
+	return 0, m3u8.ErrOpenPlaylist
+}
+
+func (f fakeSource) Close() error {
+	return nil
+}
+
+func TestParsePlaylist(t *testing.T) {
+	type testCaseParams struct {
+		name     string
+		path     string
+		source   m3u8.Source
+		elements []m3u8.Node
+		error    bool
+	}
 	testCases := []testCaseParams{
 		{
-			name: "Parse MasterPlaylist playlist with variants",
+			name:     "Error parsing playlist",
+			path:     "not exist",
+			elements: nil,
+			source:   fakeSource{},
+			error:    true,
+		},
+		{
+			name:     "Missing version in master playlist",
+			path:     "./testdata/default/masterMissingVersion.m3u8",
+			elements: nil,
+			error:    true,
+		},
+		{
+			name: "Parse media playlist",
+			path: "./testdata/default/media.m3u8",
+			elements: []m3u8.Node{
+				{Tags: "3"},
+				{Tags: "360948012"},
+				{Tags: "7"},
+				{Tags: &m3u8.ProgramDateTime{DateTime: "2024-11-25T16:00:53.200000Z"}},
+				{Tags: &m3u8.Segment{Duration: "4.8", URI: "channel-audio_1=96000-video=2262976-360948204.ts"}},
+				{Tags: &m3u8.Segment{Duration: "3.3333", URI: "channel-audio_1=96000-video=2262976-360948205.ts"}},
+				{Tags: &m3u8.DateRange{
+					Id:              "1-1732551382",
+					StartDate:       "2024-11-25T16:16:22.933333Z",
+					PlannedDuration: "60.1",
+					Scte35Mark:      map[string]string{"OUT": "0xFC3025000000000BB802FFF01405000000017FEFFF1A7B3B607E005288E8000100000000F799F45B"},
+				}},
+				{Tags: &m3u8.ProgramDateTime{DateTime: "2024-11-25T16:16:22.933333Z"}},
+				{Tags: &m3u8.Segment{Duration: "6.2666", URI: "channel-audio_1=96000-video=2262976-360948206.ts"}},
+				{Tags: &m3u8.Segment{Duration: "4.8", URI: "channel-audio_1=96000-video=2262976-360948207.ts"}},
+				{Tags: &m3u8.Segment{Duration: "4.8", URI: "channel-audio_1=96000-video=2262976-360948208.ts"}},
+				{Tags: &m3u8.Segment{Duration: "4.8", URI: "channel-audio_1=96000-video=2262976-360948209.ts"}},
+				{Tags: &m3u8.Segment{Duration: "4.8", URI: "channel-audio_1=96000-video=2262976-360948210.ts"}},
+				{Tags: &m3u8.Segment{Duration: "4.8", URI: "channel-audio_1=96000-video=2262976-360948211.ts"}},
+				{Tags: &m3u8.Segment{Duration: "4.8", URI: "channel-audio_1=96000-video=2262976-360948212.ts"}},
+				{Tags: &m3u8.Segment{Duration: "4.8", URI: "channel-audio_1=96000-video=2262976-360948213.ts"}},
+				{Tags: &m3u8.Segment{Duration: "4.8", URI: "channel-audio_1=96000-video=2262976-360948214.ts"}},
+				{Tags: &m3u8.Segment{Duration: "4.8", URI: "channel-audio_1=96000-video=2262976-360948215.ts"}},
+				{Tags: &m3u8.Segment{Duration: "4.8", URI: "channel-audio_1=96000-video=2262976-360948216.ts"}},
+				{Tags: &m3u8.Segment{Duration: "5.8333", URI: "channel-audio_1=96000-video=2262976-360948217.ts"}},
+				{Tags: &m3u8.DateRange{
+					Id:         "1-1732551382",
+					StartDate:  "2024-11-25T16:16:22.933333Z",
+					EndDate:    "2024-11-25T16:17:23.033333Z",
+					Duration:   "60.1",
+					Scte35Mark: map[string]string{"IN": "0xFC3025000000000BB802FFF01405000000017F6FFF1ACDC4487E00000000000100000000E9B751B0"},
+				}},
+				{Tags: &m3u8.ProgramDateTime{DateTime: "2024-11-25T16:17:23.033333Z"}},
+				{Tags: &m3u8.Segment{Duration: "3.7666", URI: "channel-audio_1=96000-video=2262976-360948218.ts"}},
+				{Tags: &m3u8.Segment{Duration: "4.8", URI: "channel-audio_1=96000-video=2262976-360948219.ts"}},
+			},
+		},
+		{
+			name: "Parse master playlist with variants",
 			path: "./testdata/default/master.m3u8",
-			masterPlaylist: &m3u8.MasterPlaylist{
-				Version: "3",
-				Variants: []m3u8.StreamInf{
-					{
-						Codecs:           []string{"mp4a.40.2", "avc1.64001F"},
-						Bandwidth:        "206000",
-						AverageBandwidth: "187000",
-						Resolution:       "256x144",
-						FrameRate:        "30",
-						URI:              "channel-audio_1=96000-video=80000.m3u8",
-					},
-					{
-						Codecs:           []string{"mp4a.40.2", "avc1.64001F"},
-						Bandwidth:        "299000",
-						AverageBandwidth: "272000",
-						Resolution:       "384x216",
-						FrameRate:        "30",
-						URI:              "channel-audio_1=96000-video=160000.m3u8",
-					}, {
-						Codecs:           []string{"mp4a.40.2", "avc1.64001F"},
-						Bandwidth:        "479000",
-						AverageBandwidth: "435000",
-						Resolution:       "512x288",
-						FrameRate:        "30",
-						URI:              "channel-audio_1=96000-video=313984.m3u8",
-					}, {
-						Codecs:           []string{"mp4a.40.2", "avc1.64001F"},
-						Bandwidth:        "764000",
-						AverageBandwidth: "695000",
-						Resolution:       "640x360",
-						FrameRate:        "30",
-						URI:              "channel-audio_1=96000-video=558976.m3u8",
-					}, {
-						Codecs:           []string{"mp4a.40.2", "avc1.64001F"},
-						Bandwidth:        "1224000",
-						AverageBandwidth: "1112000",
-						Resolution:       "768x432",
-						FrameRate:        "30",
-						URI:              "channel-audio_1=96000-video=952960.m3u8",
-					}, {
-						Codecs:           []string{"mp4a.40.2", "avc1.64001F"},
-						Bandwidth:        "1835000",
-						AverageBandwidth: "1668000",
-						Resolution:       "1280x720",
-						FrameRate:        "30",
-						URI:              "channel-audio_1=96000-video=1476992.m3u8",
-					}, {
-						Codecs:           []string{"mp4a.40.2", "avc1.64001F"},
-						Bandwidth:        "2751000",
-						AverageBandwidth: "2501000",
-						Resolution:       "1280x720",
-						FrameRate:        "30",
-						URI:              "channel-audio_1=96000-video=2262976.m3u8",
-					}, {
-						Codecs:           []string{"mp4a.40.2", "avc1.640029"},
-						Bandwidth:        "4127000",
-						AverageBandwidth: "3752000",
-						Resolution:       "1920x1080",
-						FrameRate:        "30",
-						URI:              "channel-audio_1=96000-video=3442944.m3u8",
-					},
-				},
+			elements: []m3u8.Node{
+				{Tags: "3"},
+				{Tags: &m3u8.StreamInf{
+					Codecs:           []string{"mp4a.40.2", "avc1.64001F"},
+					Bandwidth:        "206000",
+					AverageBandwidth: "187000",
+					Resolution:       "256x144",
+					FrameRate:        "30",
+					URI:              "channel-audio_1=96000-video=80000.m3u8",
+				}},
+				{Tags: &m3u8.StreamInf{
+					Codecs:           []string{"mp4a.40.2", "avc1.64001F"},
+					Bandwidth:        "299000",
+					AverageBandwidth: "272000",
+					Resolution:       "384x216",
+					FrameRate:        "30",
+					URI:              "channel-audio_1=96000-video=160000.m3u8",
+				}},
+				{Tags: &m3u8.StreamInf{
+					Codecs:           []string{"mp4a.40.2", "avc1.64001F"},
+					Bandwidth:        "479000",
+					AverageBandwidth: "435000",
+					Resolution:       "512x288",
+					FrameRate:        "30",
+					URI:              "channel-audio_1=96000-video=313984.m3u8",
+				}},
+				{Tags: &m3u8.StreamInf{
+					Codecs:           []string{"mp4a.40.2", "avc1.64001F"},
+					Bandwidth:        "764000",
+					AverageBandwidth: "695000",
+					Resolution:       "640x360",
+					FrameRate:        "30",
+					URI:              "channel-audio_1=96000-video=558976.m3u8",
+				}},
+				{Tags: &m3u8.StreamInf{
+					Codecs:           []string{"mp4a.40.2", "avc1.64001F"},
+					Bandwidth:        "1224000",
+					AverageBandwidth: "1112000",
+					Resolution:       "768x432",
+					FrameRate:        "30",
+					URI:              "channel-audio_1=96000-video=952960.m3u8",
+				}},
+				{Tags: &m3u8.StreamInf{
+					Codecs:           []string{"mp4a.40.2", "avc1.64001F"},
+					Bandwidth:        "1835000",
+					AverageBandwidth: "1668000",
+					Resolution:       "1280x720",
+					FrameRate:        "30",
+					URI:              "channel-audio_1=96000-video=1476992.m3u8",
+				}},
+				{Tags: &m3u8.StreamInf{
+					Codecs:           []string{"mp4a.40.2", "avc1.64001F"},
+					Bandwidth:        "2751000",
+					AverageBandwidth: "2501000",
+					Resolution:       "1280x720",
+					FrameRate:        "30",
+					URI:              "channel-audio_1=96000-video=2262976.m3u8",
+				}},
+				{Tags: &m3u8.StreamInf{
+					Codecs:           []string{"mp4a.40.2", "avc1.640029"},
+					Bandwidth:        "4127000",
+					AverageBandwidth: "3752000",
+					Resolution:       "1920x1080",
+					FrameRate:        "30",
+					URI:              "channel-audio_1=96000-video=3442944.m3u8",
+				}},
 			},
 		},
 	}
 
 	for _, tc := range testCases {
+		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
-			playlist, err := m3u8.ParseMasterPlaylist(tc.path)
+			t.Parallel()
+
+			file, err := os.Open(tc.path)
+			playlist, err := m3u8.ParsePlaylist(file)
+
+			if tc.error {
+				if tc.name == "Missing version in master playlist" {
+					file, err = os.Open(tc.path)
+					playlist, err = m3u8.ParsePlaylist(file)
+					assert.ErrorContains(t, err, m3u8.ErrParseLine.Error())
+					assert.Nil(t, playlist)
+					return
+				} else if tc.name == "Error parsing playlist" {
+					playlist, err = m3u8.ParsePlaylist(tc.source)
+					assert.Equal(t, err, m3u8.ErrOpenPlaylist)
+					assert.Nil(t, playlist)
+					return
+				}
+			}
+
 			assert.NoError(t, err)
 			assert.NotNil(t, playlist)
-			assert.Equal(t, tc.masterPlaylist, playlist)
+			assert.Len(t, playlist.Elements, len(tc.elements))
+			assert.NotNil(t, playlist.Head)
+			assert.NotNil(t, playlist.Tail)
+			for i := range tc.elements {
+				assert.Equal(t, tc.elements[i].Tags, playlist.Elements[i].Tags)
+			}
 		})
 	}
 }
 
-func TestParseMediaPlaylist(t *testing.T) {
-	t.Parallel()
+func TestMediaPlaylistInsert(t *testing.T) {
 	type testCaseParams struct {
-		name          string
-		path          string
-		mediaPlaylist *m3u8.MediaPlaylist
+		name             string
+		initialState     *m3u8.Playlist
+		nodeToInsert     *m3u8.Node
+		expectedHead     *m3u8.Node
+		expectedTail     *m3u8.Node
+		expectedElements []m3u8.Node
+		expectedPrev     *m3u8.Node
+		expectedNext     *m3u8.Node
 	}
 
 	testCases := []testCaseParams{
 		{
-			name: "Parse media playlist",
-			path: "./testdata/default/media.m3u8",
-			mediaPlaylist: &m3u8.MediaPlaylist{
-				Version:        "3",
-				TargetDuration: "7",
-				MediaSequence:  "360948012",
-				DateRanges: []m3u8.DateRange{
-					{
-						Scte35Mark: map[string]string{
-							"OUT": "0xFC3025000000000BB802FFF01405000000017FEFFF1A7B3B607E005288E8000100000000F799F45B",
-						},
-						Id:              "1-1732551382",
-						StartDate:       "2024-11-25T16:16:22.933333Z",
-						EndDate:         "",
-						PlannedDuration: "60.1",
-						Duration:        "",
-					}, {
-						Scte35Mark: map[string]string{
-							"IN": "0xFC3025000000000BB802FFF01405000000017F6FFF1ACDC4487E00000000000100000000E9B751B0",
-						},
-						Id:              "1-1732551382",
-						StartDate:       "2024-11-25T16:16:22.933333Z",
-						EndDate:         "2024-11-25T16:17:23.033333Z",
-						PlannedDuration: "",
-						Duration:        "60.1",
-					},
-				},
-				ProgramsDateTime: []m3u8.ProgramDateTime{
-					{
-						DateTime: "2024-11-25T16:00:53.200000Z",
-					}, {
-						DateTime: "2024-11-25T16:16:22.933333Z",
-					}, {
-						DateTime: "2024-11-25T16:17:23.033333Z",
-					},
-				},
-				Segments: []m3u8.Segment{
-					{
-						Duration: "4.8",
-						URI:      "channel-audio_1=96000-video=2262976-360948204.ts",
-					}, {
-						Duration: "3.3333",
-						URI:      "channel-audio_1=96000-video=2262976-360948205.ts",
-					}, {
-						Duration: "6.2666",
-						URI:      "channel-audio_1=96000-video=2262976-360948206.ts",
-					}, {
-						Duration: "4.8",
-						URI:      "channel-audio_1=96000-video=2262976-360948207.ts",
-					}, {
-						Duration: "4.8",
-						URI:      "channel-audio_1=96000-video=2262976-360948208.ts",
-					}, {
-						Duration: "4.8",
-						URI:      "channel-audio_1=96000-video=2262976-360948209.ts",
-					}, {
-						Duration: "4.8",
-						URI:      "channel-audio_1=96000-video=2262976-360948210.ts",
-					}, {
-						Duration: "4.8",
-						URI:      "channel-audio_1=96000-video=2262976-360948211.ts",
-					}, {
-						Duration: "4.8",
-						URI:      "channel-audio_1=96000-video=2262976-360948212.ts",
-					}, {
-						Duration: "4.8",
-						URI:      "channel-audio_1=96000-video=2262976-360948213.ts",
-					}, {
-						Duration: "4.8",
-						URI:      "channel-audio_1=96000-video=2262976-360948214.ts",
-					}, {
-						Duration: "4.8",
-						URI:      "channel-audio_1=96000-video=2262976-360948215.ts",
-					}, {
-						Duration: "4.8",
-						URI:      "channel-audio_1=96000-video=2262976-360948216.ts",
-					}, {
-						Duration: "5.8333",
-						URI:      "channel-audio_1=96000-video=2262976-360948217.ts",
-					}, {
-						Duration: "3.7666",
-						URI:      "channel-audio_1=96000-video=2262976-360948218.ts",
-					}, {
-						Duration: "4.8",
-						URI:      "channel-audio_1=96000-video=2262976-360948219.ts",
-					},
-				},
+			name:             "Empty list, first node insertion",
+			initialState:     &m3u8.Playlist{},
+			nodeToInsert:     &m3u8.Node{Tags: "3"},
+			expectedHead:     &m3u8.Node{Tags: "3"},
+			expectedTail:     &m3u8.Node{Tags: "3"},
+			expectedElements: []m3u8.Node{{Tags: "3"}},
+			expectedPrev:     nil,
+			expectedNext:     nil,
+		},
+		{
+			name: "Existing list, second node insertion",
+			initialState: &m3u8.Playlist{
+				Head:     &m3u8.Node{Tags: "3"},
+				Tail:     &m3u8.Node{Tags: "3"},
+				Elements: []m3u8.Node{{Tags: "3"}},
 			},
+			nodeToInsert: &m3u8.Node{
+				Tags: "360948012",
+			},
+			expectedHead: &m3u8.Node{Tags: "3"},
+			expectedTail: &m3u8.Node{Tags: "360948012"},
+			expectedElements: []m3u8.Node{
+				{Tags: "3"},
+				{Tags: "360948012"},
+			},
+			expectedPrev: &m3u8.Node{Tags: "3"},
+			expectedNext: &m3u8.Node{Tags: "360948012"},
 		},
 	}
 
 	for _, tc := range testCases {
+		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
-			playlist, err := m3u8.ParseMediaPlaylist(tc.path)
-			assert.NoError(t, err)
-			assert.NotNil(t, playlist)
-			assert.Equal(t, tc.mediaPlaylist, playlist)
+			t.Parallel()
+			playlist := tc.initialState
+			nodeToInsert := tc.nodeToInsert
+			playlist.Insert(nodeToInsert)
+
+			assert.Equal(t, tc.expectedHead.Tags, playlist.Head.Tags)
+			assert.Equal(t, tc.expectedTail.Tags, playlist.Tail.Tags)
+
+			for i := range tc.expectedElements {
+				assert.Equal(t, tc.expectedElements[i].Tags, playlist.Elements[i].Tags)
+			}
+
+			if tc.name == "Existing list, second node insertion" {
+				assert.Equal(t, tc.expectedPrev.Tags, nodeToInsert.Prev.Tags)
+				assert.Nil(t, nodeToInsert.Next)
+			}
 		})
 	}
+}
+
+func TestMediaPlaylistFind(t *testing.T) {
+	type testCaseParams struct {
+		name        string
+		playlist    *m3u8.Playlist
+		expectedTag any
+	}
+	testCases := []testCaseParams{
+		{
+			name:        "Empty playlist",
+			playlist:    &m3u8.Playlist{},
+			expectedTag: nil,
+		},
+		{
+			name: "Existing playlist",
+			playlist: &m3u8.Playlist{
+				Head: &m3u8.Node{
+					Tags: "3",
+					Next: &m3u8.Node{
+						Tags: "360948012",
+					},
+				},
+				Tail: &m3u8.Node{
+					Tags: "360948012",
+				},
+				Elements: []m3u8.Node{
+					{Tags: "3"},
+					{Tags: "360948012"},
+				},
+			},
+			expectedTag: "360948012",
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			node, found := tc.playlist.Find(tc.expectedTag)
+			if tc.expectedTag != nil {
+				assert.Equal(t, tc.expectedTag, node.Tags)
+				assert.True(t, found)
+			} else {
+				assert.Nil(t, node)
+				assert.False(t, found)
+			}
+		})
+	}
+
 }
