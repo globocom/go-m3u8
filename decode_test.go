e6f4c3baec8dc444ffaf9532a794549583159739
@bellamariz <isabellafmariz@gmail.com>
2025-05-09 17:05:56 -0300

    test: Add tests for media playlists with ad breaks to decode test suite and add TODO for bug fix

--- decode_test.go
+++ decode_test.go
@@ -205,7 +205,7 @@ func TestMultiLineHLSElements_StreamInf(t *testing.T) {
 	assert.Equal(t, "30", node.HLSElement.Attrs["FRAME-RATE"])
 }
 
-func TestCompleteMultivariantPlaylist(t *testing.T) {
+func TestParseMultivariantPlaylist(t *testing.T) {
 	playlist := `#EXTM3U
 							#EXT-X-VERSION:3
 							## Created with Unified Streaming Platform  (version=1.14.4-30793)
@@ -226,23 +226,51 @@ func TestCompleteMultivariantPlaylist(t *testing.T) {
 	assert.Nil(t, p.CurrentStreamInf)
 }
 
-func TestCompleteMediaPlaylist(t *testing.T) {
-	playlist := `#EXTM3U
-							#EXT-X-VERSION:3
-							## Created with Unified Streaming Platform  (version=1.14.4-30793)
-
-							# variants
-							#EXT-X-STREAM-INF:BANDWIDTH=759000,AVERAGE-BANDWIDTH=690000,CODECS=\"mp4a.40.2,avc1.64001F\",RESOLUTION=640x360,FRAME-RATE=30
-							coelhodai-audio_1=96000-video=558976.m3u8?dvr_window_length=600
-							#EXT-X-STREAM-INF:BANDWIDTH=759000,AVERAGE-BANDWIDTH=690000,CODECS=\"mp4a.40.2,avc1.64001F\",RESOLUTION=720x480,FRAME-RATE=30
-							coelhodai-audio_1=96000-video=123456.m3u8?dvr_window_length=600`
-
-	p, err := setupPlaylist(playlist)
+func TestParseMediaPlaylist(t *testing.T) {
+	file, _ := os.Open("testdata/media/media.m3u8")
+	p, err := m3u8.ParsePlaylist(file)
 
 	assert.NoError(t, err)
 	assert.Equal(t, p.Head.HLSElement.Name, "M3u8Identifier")
-	assert.Equal(t, p.Tail.HLSElement.Name, "StreamInf")
-	assert.NotEqual(t, p.Variants(), 0)
+	assert.Equal(t, p.Tail.HLSElement.Name, "ExtInf")
+	assert.Equal(t, len(p.Segments()), 16)
+	assert.Nil(t, p.CurrentSegment)
+	assert.Nil(t, p.CurrentStreamInf)
+}
+
+func TestParseMediaPlaylist_WithAdBreak(t *testing.T) {
+	file, _ := os.Open("testdata/media/withAdBreak.m3u8")
+	p, err := m3u8.ParsePlaylist(file)
+
+	p.Print()
+
+	cueIn, foundCueIn := p.Find("CueIn")
+	breaks := p.Breaks()
+
+	assert.NoError(t, err)
+	assert.True(t, foundCueIn)
+	assert.Equal(t, cueIn.HLSElement.Name, "CueIn")
+	assert.Equal(t, len(breaks), 1)
+	assert.Equal(t, breaks[0].HLSElement.Details["MediaSequence"], "363903241")
+	assert.Nil(t, p.CurrentSegment)
+	assert.Nil(t, p.CurrentStreamInf)
+}
+
+func TestParseMediaPlaylist_WithAdBreakOutsideDVR(t *testing.T) {
+	file, _ := os.Open("testdata/media/withAdBreakOutsideDVR.m3u8")
+	p, err := m3u8.ParsePlaylist(file)
+
+	cueIn, foundCueIn := p.Find("CueIn")
+	breaks := p.Breaks()
+
+	assert.NoError(t, err)
+	assert.True(t, foundCueIn)
+	assert.Equal(t, cueIn.HLSElement.Name, "CueIn")
+	assert.Equal(t, len(breaks), 1)
+	// TODO: We do not want the daterange's media sequence to change
+	// It should always be equal to the break's first segment's media sequence
+	// In this example, it should remain 363903241, and not increment 363903242
+	assert.Equal(t, breaks[0].HLSElement.Details["MediaSequence"], "363903242")
 	assert.Nil(t, p.CurrentSegment)
 	assert.Nil(t, p.CurrentStreamInf)
 }
