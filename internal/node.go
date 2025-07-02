1c28ef367df4e10c627c039c9caa618a674e7d80
@bellamariz <isabellafmariz@gmail.com>
2025-07-02 15:34:21 -0300

    feat: Create new node.go methods for inserting nodes into linked list

--- internal/node.go
+++ internal/node.go
@@ -32,6 +32,7 @@ type HLSElement struct {
 	Details map[string]string
 }
 
+// Insert adds a new node to the end of the doubly linked list
 func (l *DoublyLinkedList) Insert(node *Node) {
 	if l.Head == nil {
 		l.Head = node
@@ -43,6 +44,63 @@ func (l *DoublyLinkedList) Insert(node *Node) {
 	}
 }
 
+// InsertAfter inserts newNode after node in the doubly linked list
+//
+//	node ---> newNode ---> node.Next
+func (l *DoublyLinkedList) InsertAfter(node *Node, newNode *Node) {
+	if node == nil {
+		return
+	}
+
+	newNode.Prev = node
+	newNode.Next = node.Next
+	node.Next = newNode
+
+	if newNode.Next != nil {
+		newNode.Next.Prev = newNode
+	} else {
+		l.Tail = newNode
+	}
+}
+
+// InsertBefore inserts newNode before node in the doubly linked list
+//
+//	node.Prev ---> newNode ---> node
+func (l *DoublyLinkedList) InsertBefore(node *Node, newNode *Node) {
+	if node == nil {
+		return
+	}
+
+	newNode.Next = node
+	newNode.Prev = node.Prev
+	node.Prev = newNode
+
+	if newNode.Prev != nil {
+		newNode.Prev.Next = newNode
+	} else {
+		l.Head = newNode
+	}
+}
+
+// InsertBetween inserts newNode between node1 and node2 in the doubly linked list
+//
+//	node1 ---> newNode ---> node2
+func (l *DoublyLinkedList) InsertBetween(node1 *Node, node2 *Node, newNode *Node) {
+	if node1 == nil || node2 == nil {
+		return
+	}
+
+	if node1.Next != node2 {
+		return
+	}
+
+	newNode.Prev = node1
+	newNode.Next = node2
+	node1.Next = newNode
+	node2.Prev = newNode
+}
+
+// Find searches for a node with the specified element name in the doubly linked list
 func (l *DoublyLinkedList) Find(elementName string) (*Node, bool) {
 	current := l.Head
 	for current != nil {
@@ -55,6 +113,7 @@ func (l *DoublyLinkedList) Find(elementName string) (*Node, bool) {
 	return nil, false
 }
 
+// FindAll searches for all nodes with the specified element name in the doubly linked list
 func (l *DoublyLinkedList) FindAll(elementName string) []*Node {
 	current := l.Head
 	result := make([]*Node, 0)
@@ -67,6 +126,7 @@ func (l *DoublyLinkedList) FindAll(elementName string) []*Node {
 	return result
 }
 
+// ModifyNodesBetween modifies nodes in the doubly linked list between start and end conditions
 func (l *DoublyLinkedList) ModifyNodesBetween(
 	startCondition func(*Node) bool,
 	endCondition func(*Node) bool,
