0f3b99b31cd58c89c3aae28c321c51fde2894751
Maria-Ferreira <marigaspaar03@gmail.com>
2025-10-20 18:30:42 -0300

    fix: correct rebase mistakes

--- playlist/playlist.go
+++ playlist/playlist.go
@@ -325,3 +325,15 @@ func (p *Playlist) removeDuplicateBreakTags(adBreak *internal.Node) {
 
 	p.Remove(adBreak)
 }
+
+// Returns the previous segment (#EXTINF) before the given node, or nil if none exists.
+func (p *Playlist) FindPreviousSegment(node *internal.Node) *internal.Node {
+	current := node.Prev
+	for current != nil {
+		if current.HLSElement.Name == "ExtInf" {
+			return current
+		}
+		current = current.Prev
+	}
+	return nil
+}
