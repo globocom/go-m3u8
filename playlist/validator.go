8fe6af2fc64327bcd5d59278b926cb5f2d427e90
cynthia.tsai <cynthia.tsai@g.globo>
2025-11-06 17:23:24 -0300

    use media sequence for cue in

--- playlist/validator.go
+++ playlist/validator.go
@@ -80,7 +80,7 @@ func (p *Playlist) IsCueInFromBreak(breakMediaSequence int, currentElement *inte
 
 	if previousSegment != nil {
 		node, found := p.FindNodeInsideAdBreak(previousSegment)
-		nodeMediaSequence, _ := strconv.Atoi(node.HLSElement.Details["StartMediaSequence"])
+		nodeMediaSequence, _ := strconv.Atoi(node.HLSElement.Details["MediaSequence"])
 		if found {
 			if nodeMediaSequence == breakMediaSequence {
 				return true
