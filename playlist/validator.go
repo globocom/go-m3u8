5bad924a5303b9a1690afe33f7ebedbaf2f161b9
cynthia.tsai <cynthia.tsai@g.globo>
2025-11-06 17:26:21 -0300

    .

--- playlist/validator.go
+++ playlist/validator.go
@@ -80,7 +80,7 @@ func (p *Playlist) IsCueInFromBreak(breakMediaSequence int, currentElement *inte
 
 	if previousSegment != nil {
 		node, found := p.FindNodeInsideAdBreak(previousSegment)
-		nodeMediaSequence, _ := strconv.Atoi(node.HLSElement.Details["MediaSequence"])
+		nodeMediaSequence, _ := strconv.Atoi(node.HLSElement.Details["StartMediaSequence"])
 		if found {
 			if nodeMediaSequence == breakMediaSequence {
 				return true
